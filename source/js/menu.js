207323abb194b2df25a8f8fbbd2a41008de7222d
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2021-04-28 20:58:13 +0900

    Выполнена ретинизация и оптимизация фоновых картинок секции О приложении, исправлена работа меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,21 +1,21 @@
-const headerWrapper = document.querySelector(".header__wrapper");
+const header = document.querySelector(".header");
 const menuToggle = document.querySelector(".header__menu-toggle");
 const mainMenu = document.querySelector(".menu");
 
 menuToggle.classList.remove("header__menu-toggle--nojs");
-headerWrapper.classList.remove("header__wrapper--opened");
+header.classList.remove("header--menu-opened");
 mainMenu.classList.add("menu--closed");
 
 menuToggle.addEventListener("click", function() {
   if (mainMenu.classList.contains("menu--closed")) {
     menuToggle.classList.remove("header__menu-toggle--closed");
     menuToggle.classList.add("header__menu-toggle--opened");
-    headerWrapper.classList.add("header__wrapper--opened");
+    header.classList.add("header--menu-opened");
     mainMenu.classList.remove("menu--closed");
   } else {
     menuToggle.classList.remove("header__menu-toggle--opened");
     menuToggle.classList.add("header__menu-toggle--closed");
-    headerWrapper.classList.remove("header__wrapper--opened");
+    header.classList.remove("header--menu-opened");
     mainMenu.classList.add("menu--closed");
   }
 });
