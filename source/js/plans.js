55883f4ddbde725ebf44eb810399d804d5fbf832
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2021-05-12 20:22:42 +0900

    Добавлено вычисление положения полосы прокрутки в блоке Планов на главной

--- source/js/plans.js
+++ source/js/plans.js
@@ -1,3 +1,14 @@
 const plansList = document.querySelector(".plans__list");
 
-plansList.scrollLeft = 256;
+let plansListWidth = plansList.scrollWidth;
+
+let setScrollPosition = function () {
+  let scrollPosition = (plansListWidth / 2) - (window.innerWidth / 2);
+  plansList.scrollTo(scrollPosition, 0);
+}
+
+setScrollPosition();
+
+window.onresize = function () {
+  setScrollPosition();
+}
